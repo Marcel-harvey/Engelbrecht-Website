b991059c3d8a170cba6c3a310882c6240c088976
Marcel Harvey <marcelharvey1@gmail.com>
2026-03-11 18:58:31 +0200

    Create a navbar
    - Create a modern and responsive navbar for the website
    - Use CSS global variables for colors

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
-  selector: 'app-navbar.component',
+  selector: 'app-navbar',
   imports: [],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
+
+  menuOpen = signal<boolean>(false);
+
+  toggleMenu() {
+    this.menuOpen.update(m => !m);
+  }
 
 }
