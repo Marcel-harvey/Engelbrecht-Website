5393be7bb080f20571ce34e5c5ffa11f8df8ba1d
Marcel Harvey <marcelharvey1@gmail.com>
2026-03-11 19:01:19 +0200

    Add new feature
    - Create new feature for previewing all services that company provides

---
+++ src/app/features/home/components/services-preview/services-preview.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-services-preview.component',
+  imports: [],
+  templateUrl: './services-preview.component.html',
+  styleUrl: './services-preview.component.css',
+})
+export class ServicesPreviewComponent {
+
+}
