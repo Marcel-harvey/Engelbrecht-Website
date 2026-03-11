0a75c77f06be2d83d3ebe49440c6f3c6ca161072
Marcel Harvey <marcelharvey1@gmail.com>
2026-03-11 18:57:12 +0200

    Create route for all components
    - Create new routes for all components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
+import { AboutComponent } from './features/about/about.component';
+import { ServicesComponent } from './features/services/services.component';
+import { BookingComponent } from './features/booking/booking.component';
+import { ContactComponent } from './features/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{ path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'booking', component: BookingComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', redirectTo: '' }
+];
