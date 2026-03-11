c6c346a96024c49b7f61808198e4f832adc0d4ee
Marcel Harvey <marcelharvey1@gmail.com>
2026-03-11 19:02:44 +0200

    Add new feature
    - Create new feature for about section

---
+++ src/app/features/home/components/about-preview/about-preview.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-preview.component',
+  imports: [],
+  templateUrl: './about-preview.component.html',
+  styleUrl: './about-preview.component.css',
+})
+export class AboutPreviewComponent {
+
+}
