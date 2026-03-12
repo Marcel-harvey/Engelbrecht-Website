89423d28ddc0038608709f29636967da540a1d10
Marcel Harvey <marcelharvey1@gmail.com>
2026-03-12 13:19:44 +0200

    Add new feature
    - Create new feature for location map

---
+++ src/app/features/home/components/location-map/location-map.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-location-map.component',
+  imports: [],
+  templateUrl: './location-map.component.html',
+  styleUrl: './location-map.component.css',
+})
+export class LocationMapComponent {
+
+}
