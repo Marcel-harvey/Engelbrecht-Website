33211a5584157c1157c0222a728edda86ff60730
Marcel Harvey <marcelharvey1@gmail.com>
2026-03-12 14:21:17 +0200

    Add child components
    - Add all child components related to the home age
    - Hero component
    - About component
    - testimonials component
    - call to action component
    - FAQ component
    - location map component

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { HeroComponent } from './components/hero/hero.component';
+import { ServicesPreviewComponent } from './components/services-preview/services-preview.component';
+import { AboutPreviewComponent } from './components/about-preview/about-preview.component';
+import { TestimonialsComponent } from './components/testimonials/testimonials.component';
+import { CallToActionComponent } from './components/call-to-action/call-to-action.component';
+import { FaqComponent } from "./components/faq/faq.component";
+import { LocationMapComponent } from "./components/location-map/location-map.component";
 
 @Component({
-  selector: 'app-home.component',
-  imports: [],
+  selector: 'app-home',
+  imports: [HeroComponent, ServicesPreviewComponent, AboutPreviewComponent, TestimonialsComponent, CallToActionComponent, FaqComponent, LocationMapComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css',
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
