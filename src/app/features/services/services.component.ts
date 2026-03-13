9d525dd9141d9974cc6723e502df2c376f18f09f
Marcel Harvey <marcelharvey1@gmail.com>
2026-03-13 12:43:12 +0200

    Add service page
    - Create new service page
    - Contain all services provided by company

--- src/app/features/services/services.component.ts
+++ src/app/features/services/services.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ServiceService } from '../../shared/services/services.service';
+import { ServiceInterface } from '../../shared/models/service.model';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-services.component',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './services.component.html',
   styleUrl: './services.component.css',
 })
 export class ServicesComponent {
 
+  private serviceService = inject(ServiceService);
+
+  services: ServiceInterface[] = [];
+  featured: ServiceInterface[] = [];
+
+  ngOnInit() {
+    this.services = this.serviceService.getAllServices();
+    this.featured = this.services.filter(s => s.popular);
+  }
 }
