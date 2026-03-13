81279793c987f5c659e9444c7652b9b31ca4ece3
Marcel Harvey <marcelharvey1@gmail.com>
2026-03-13 19:04:12 +0200

    Add form for booking
    - Create a form for booking a massage
    - Use reactive forms
    - Add dummy submit

--- src/app/features/booking/booking.component.ts
+++ src/app/features/booking/booking.component.ts
@@ -1,11 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ServiceInterface } from '../../shared/models/service.model';
+import { ServiceService } from '../../shared/services/services.service';
 
 @Component({
-  selector: 'app-booking.component',
-  imports: [],
+  selector: 'app-booking',
+  imports: [ReactiveFormsModule],
   templateUrl: './booking.component.html',
-  styleUrl: './booking.component.css',
+  styleUrls: ['./booking.component.css']
 })
-export class BookingComponent {
+export class BookingComponent implements OnInit {
+  // Depandancies
+  private readonly fb = inject(FormBuilder);
+  private readonly _service = inject(ServiceService);
 
+  // UI State
+  readonly submitted = signal<boolean>(false);
+  readonly errorMessage = signal<string>('');
+  successMessage = '';
+  readonly isLoading = signal<boolean>(false);
+
+  readonly services = signal<ServiceInterface[] | null>(null);
+
+    bookingForm = this.fb.group({
+      fullName: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', [Validators.required, Validators.pattern(/^\+?\d{10,15}$/)]],
+      service: ['', Validators.required],
+      date: ['', Validators.required],
+      time: ['', Validators.required],
+      notes: ['']
+    });
+
+  ngOnInit(): void {
+    this.services.set(this._service.getAllServices());
+  }
+
+  get formControl() {
+    return this.bookingForm.controls;
+  }
+
+  async onSubmit(): Promise<void> {
+    this.submitted.set(true);
+
+    if (this.bookingForm.invalid) {
+      return;
+    }
+
+    const bookingData = this.bookingForm.value;
+    console.log('Booking Data:', bookingData);
+
+    // Placeholder for backend integration
+    this.successMessage = 'Your booking request has been sent successfully!';
+    this.bookingForm.reset();
+    this.submitted.set(false);
+  }
 }
