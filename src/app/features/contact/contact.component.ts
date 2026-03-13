129efa1147d015a684a60f2188e712f373be8cab
Marcel Harvey <marcelharvey1@gmail.com>
2026-03-14 00:51:35 +0200

    Add contact form
    - Create new form for contact component
    - Add contact infomration
    - Add location Map with company adress as pin drop

--- src/app/features/contact/contact.component.ts
+++ src/app/features/contact/contact.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-contact.component',
-  imports: [],
+  selector: 'app-contact',
+  imports: [ReactiveFormsModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css',
+  styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
+  // Dependancies
+  private readonly fb = inject(FormBuilder)
 
+  readonly submitted = signal<boolean>(false);
+
+  contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', Validators.required],
+      message: ['', Validators.required]
+  });
+
+  submitForm() {
+    this.submitted.set(true);
+
+    if (this.contactForm.invalid) {
+      return;
+    }
+
+    console.log(this.contactForm.value);
+
+    // Backend email logic will be wired later
+    alert("Thank you! Your message has been sent.");
+
+    this.contactForm.reset();
+    this.submitted.set(false);
+  }
 }
