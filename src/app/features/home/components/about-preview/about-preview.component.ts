78c21b3e4f4b5711e010fb605b6dcda387e46c91
Marcel Harvey <marcelharvey1@gmail.com>
2026-03-14 21:04:23 +0200

    Change card layout style
    - Create generic card layout style and apply to all components

--- src/app/features/home/components/about-preview/about-preview.component.ts
+++ src/app/features/home/components/about-preview/about-preview.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-about-preview',
   standalone: true,
+  imports: [RouterLink],
   templateUrl: './about-preview.component.html',
   styleUrls: ['./about-preview.component.css']
 })
