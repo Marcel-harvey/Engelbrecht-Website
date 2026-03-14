dc3a679288e2f99f6ccd4b1678a31a019e154011
Marcel Harvey <marcelharvey1@gmail.com>
2026-03-14 16:45:37 +0200

    Remove un used component
    - Move component to shared components folder

--- src/app/features/home/components/call-to-action/call-to-action.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-call-to-action',
-  standalone: true,
-  templateUrl: './call-to-action.component.html',
-  styleUrls: ['./call-to-action.component.css']
-})
-export class CallToActionComponent {}
