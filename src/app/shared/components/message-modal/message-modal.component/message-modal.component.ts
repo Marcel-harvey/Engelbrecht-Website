eb2f3b8860872bfe5b7656556c60523c0b495210
Marcel Harvey <marcelharvey1@gmail.com>
2026-03-19 13:41:44 +0200

    Add model for message
    - Create new interface for message
    - Use in modal
    - Hard code types

--- src/app/shared/components/message-modal/message-modal.component/message-modal.component.ts
+++ src/app/shared/components/message-modal/message-modal.component/message-modal.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject, input, OnInit, output, signal } from '@angular/core';
+import { MessageInterface } from '../../../models/message.model';
+import { NgClass } from '@angular/common';
+import { MessageService } from '../../../services/modal/message.service';
 
 @Component({
   selector: 'app-message-modal',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './message-modal.component.html',
   styleUrls: ['./message-modal.component.css'],
 })
 export class MessageModalComponent {
+  // Dependandcies
+  private readonly _modal = inject(MessageService);
+  
+  readonly message = this._modal.message
 
+  onConfirm(): void {
+    this._modal.close()
+  }
+
+  onBackdropClick(): void {
+    this._modal.close()
+  }
 }
